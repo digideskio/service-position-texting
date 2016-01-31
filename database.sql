0392dbcda7a59a1ed237fcc2b5a7b65ec7b46d90
Wes Cossick <wes@nextstepwebs.com>
2016-01-31 15:30:24 -0600

    Add auto increment columns

--- database.sql
+++ database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 14, 2016 at 11:31 PM
+-- Generation Time: Jan 31, 2016 at 02:29 PM
 -- Server version: 5.5.45-cll-lve
 -- PHP Version: 5.4.31
 
@@ -27,12 +27,13 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `assignments` (
-  `assignment_id` int(11) NOT NULL,
+  `assignment_id` int(11) NOT NULL AUTO_INCREMENT,
   `position_id` int(11) NOT NULL,
   `leader_id` int(11) NOT NULL,
   `was_there` tinyint(1) NOT NULL,
-  `confirmed_text` tinyint(4) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `confirmed_text` tinyint(4) NOT NULL,
+  PRIMARY KEY (`assignment_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -41,10 +42,11 @@ CREATE TABLE IF NOT EXISTS `assignments` (
 --
 
 CREATE TABLE IF NOT EXISTS `events` (
-  `event_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL AUTO_INCREMENT,
   `name` text NOT NULL,
-  `date` date NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `date` date NOT NULL,
+  PRIMARY KEY (`event_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -53,14 +55,15 @@ CREATE TABLE IF NOT EXISTS `events` (
 --
 
 CREATE TABLE IF NOT EXISTS `leaders` (
-  `leader_id` int(11) NOT NULL,
+  `leader_id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `cell_phone` varchar(20) NOT NULL,
   `email` varchar(255) NOT NULL,
   `active` tinyint(1) NOT NULL,
-  `last_text_re_assignment_id` int(11) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `last_text_re_assignment_id` int(11) NOT NULL,
+  PRIMARY KEY (`leader_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -69,11 +72,12 @@ CREATE TABLE IF NOT EXISTS `leaders` (
 --
 
 CREATE TABLE IF NOT EXISTS `positions` (
-  `position_id` int(11) NOT NULL,
+  `position_id` int(11) NOT NULL AUTO_INCREMENT,
   `event_id` int(11) NOT NULL,
   `title` text NOT NULL,
-  `notify_night` tinyint(1) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `notify_night` tinyint(1) NOT NULL,
+  PRIMARY KEY (`position_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
